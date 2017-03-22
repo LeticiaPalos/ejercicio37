9690ce1ba3a5c445e063cf0396925408440f8bbd
Leticia Palos <letizia.palos@gmail.com>
2017-03-21 22:40:21 -0600

    Ultimo commit

--- app/controllers/pags_controller.rb
+++ app/controllers/pags_controller.rb
@@ -1,7 +1,18 @@
 class PagsController < ApplicationController
+  before_action :get_data
+
   def pag1
   end
 
   def pag2
+    render :layout => 'landing'
+  end
+
+  def pag3
+
+  end
+
+  def get_data
+    @users = ["Alumno1","Alumno2","Alumnos3"]
   end
 end
